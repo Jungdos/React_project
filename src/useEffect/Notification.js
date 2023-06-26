fdb2798e9551ccc8701b72e3bc855c7e1cb42b54
Junwon <was607412@naver.com>
2023-06-26 17:16:57 +0900

    Notification 3Click

--- src/useEffect/Notification.js
+++ src/useEffect/Notification.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Notification = ({ message, show, onClose }) => (
+const Notification = ({ show, onClose }) => {
+  const [clickCount, setClickCount] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    if (clickCount >= 3) {
+      setVisible(false);
+      setTimeout(onClose, 1000);
+    }
+  }, [clickCount, onClose]);
+
+  const handleClick = () => {
+    setClickCount(prevCount => prevCount + 1);
+  };
+
+  let message;
+  switch (clickCount) {
+    case 1:
+      message = "사실 한번더 눌러야해요";
+      break;
+    case 2:
+      message = "진짜로 누르면 사라져요";
+      break;
+    default:
+      message = "누르면 사라져요";
+  }
+
+  return (
     <div 
-    onClick={onClose}
-    style={{ 
-      position: 'fixed', 
-      bottom: '20px', 
-      right: '20px', 
-      backgroundColor: '#edb41b', 
-      padding: '10px', 
-      transform: show ? 'translateX(0)' : 'translateX(100%)',
-      transition: 'transform 0.3s ease-out',
-      color: 'white',
-      fontSize: '18px',
-      fontWeight: 'bold',
-      cursor: 'pointer',
-      borderRadius: '20px'
-      }}>
-        {message}
+      onClick={handleClick}
+      style={{ 
+        position: 'fixed', 
+        bottom: '20px', 
+        right: '20px', 
+        backgroundColor: '#edb41b', 
+        padding: '10px', 
+        color: 'white',
+        fontSize: '18px',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+        borderRadius: '20px',
+        opacity: visible ? '1' : '0',
+        transition: 'opacity 1s ease-in-out'
+      }}
+    >
+      {message}
     </div>
-);
+  );
+};
 
 export default Notification;
