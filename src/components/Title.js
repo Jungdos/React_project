40ff586db4345090a870e3fdf28575639d8403b5
Junwon <was607412@naver.com>
2023-06-27 15:50:37 +0900

    Add Logo to gitjub_Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 
 const Title = () => {
     return(
-        <h2 className='h2_Title' 
-        style={ { 
-          textAlign: 'center',
-          fontSize: '3rem',
-          fontWeight: 'bold',
-          color: 'orange',
-                    }}>
-              Github_Practice
+      <div style={ {
+        display: 'flex',
+        textAlign: 'center',
+        flexDirection: 'column'
+      }}>
+        <h1>
+          <a href="https://github.com/Jungdos/React_project">
+            <img className='github'
+            src='./images/github.png' 
+            style={ {
+              width: '50px',
+              height: '50px',
+              margin: 0,
+              padding: 0
+            }}></img>
+          </a>
+        </h1>
+        <h2>
+         <Button variant="dark" 
+         style={{
+          width: '200px'
+         }}>Github</Button>
         </h2>
+        </div>
     )
 }
 
